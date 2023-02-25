f6f711db5a81bfed04795e5b45d83c43556f68fe
Mix200208 <Yaroslav_mi@mail.ru>
2023-02-25 17:19:26 +0300

    Created Login and Sing In UI

--- app/src/main/java/com/app/effectivemobiletest/ui/LogInActivity.kt
+++ app/src/main/java/com/app/effectivemobiletest/ui/LogInActivity.kt
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.app.effectivemobiletest.R
-import com.app.effectivemobiletest.databinding.ActivitySingInBinding
+import com.app.effectivemobiletest.databinding.ActivityLogInBinding
+import com.app.effectivemobiletest.databinding.ActivityMainBinding
 
-class SingInActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding: ActivitySingInBinding = DataBindingUtil.setContentView(this,R.layout.activity_sing_in)
-
+        var binding: ActivityLogInBinding = DataBindingUtil.setContentView(this,R.layout.activity_log_in)
 
 
 
--- app/src/main/java/com/app/effectivemobiletest/ui/MainActivity.kt
+++ app/src/main/java/com/app/effectivemobiletest/ui/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.app.effectivemobiletest.ui
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.app.effectivemobiletest.R
+import com.app.effectivemobiletest.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    private val uiScope = CoroutineScope(Dispatchers.Main + Job())
+    private val ioScope = CoroutineScope(Dispatchers.IO + Job())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
+
+        toMainActivity()
+    }
+
+    override fun onDestroy() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(true)
+        }
+
+        super.onDestroy()
+    }
+
+
+    private fun toMainActivity() {
+        ioScope.launch {
+            delay(3000)
+            uiScope.launch {
+                startActivity(Intent(this@MainActivity, SingInActivity::class.java))
+                finish()
+            }
+        }
     }
 }
