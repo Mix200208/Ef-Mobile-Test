62df7da4fed1c87242fcbfe9426a70241511ca74
Mix200208 <Yaroslav_mi@mail.ru>
2023-02-25 15:28:45 +0300

    Initial commit

---
+++ app/src/main/java/com/app/effectivemobiletest/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.effectivemobiletest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.effectivemobiletest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/app/effectivemobiletest/ui/SingInActivity.kt
@@ -0,0 +1,12 @@
+package com.app.effectivemobiletest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.effectivemobiletest.R
+
+class SingInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_in)
+    }
+}
