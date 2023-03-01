98c96492c1fcc3b756be1d05d88327c6dd0afe0c
Mix200208 <Yaroslav_mi@mail.ru>
2023-03-01 23:35:37 +0300

    Created Login and Sing In logic

--- app/src/main/java/com/app/effectivemobiletest/ui/LogInActivity.kt
+++ app/src/main/java/com/app/effectivemobiletest/ui/LogInActivity.kt
@@ -1,17 +1,56 @@
 package com.app.effectivemobiletest.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.app.effectivemobiletest.R
 import com.app.effectivemobiletest.databinding.ActivityLogInBinding
 import com.app.effectivemobiletest.databinding.ActivityMainBinding
+import com.google.firebase.database.*
 
 class LogInActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLogInBinding
+    var databaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://registration-db-a9167-default-rtdb.firebaseio.com/")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding: ActivityLogInBinding = DataBindingUtil.setContentView(this,R.layout.activity_log_in)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_log_in)
 
+        stUpUi()
+
+    }
+
+    fun stUpUi(){
+        binding.button.apply {
+            setOnClickListener {
+                databaseReference.apply {
+                    child("user").addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            if (snapshot.hasChild(binding.emailInput.text.toString())) {
+
+                                var password = snapshot.child(binding.emailInput.text.toString()).child("password").getValue(String::class.java)
+                                if (password!! == binding.passwordInput.text.toString()){
+                                    startActivity(Intent(this@LogInActivity,ProfileActivity::class.java))
+
+                                }else{
+                                    Toast.makeText(this@LogInActivity,"invalid password ",Toast.LENGTH_SHORT).show()
+                                }
+                            } else {
+                                Toast.makeText(this@LogInActivity,"invalid login ",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
+
+
+                    })
+                }
+            }
+        }
 
 
     }
--- app/src/main/java/com/app/effectivemobiletest/ui/SingInActivity.kt
+++ app/src/main/java/com/app/effectivemobiletest/ui/SingInActivity.kt
@@ -1,18 +1,82 @@
 package com.app.effectivemobiletest.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.app.effectivemobiletest.R
 import com.app.effectivemobiletest.databinding.ActivitySingInBinding
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
 class SingInActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySingInBinding
+
+    var database: DatabaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://registration-db-a9167-default-rtdb.firebaseio.com/")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding: ActivitySingInBinding = DataBindingUtil.setContentView(this,R.layout.activity_sing_in)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sing_in)
 
 
-
-
+        stUpUi()
     }
+
+    private fun stUpUi(){
+
+        binding.singInBut.apply {
+            setOnClickListener {
+
+                if(binding.firstNameInput.text.isNotEmpty()) {
+                    database.apply {
+                        child("user").addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                if (snapshot.hasChild(binding.emailInput.text.toString())) {
+                                    Toast.makeText(
+                                        this@SingInActivity,
+                                        "has already email",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } else {
+                                    this@apply.child("user")
+                                        .child(binding.emailInput.text.toString()).child("name")
+                                        .setValue(binding.firstNameInput.text.toString())
+                                    this@apply.child("user")
+                                        .child(binding.emailInput.text.toString()).child("surname")
+                                        .setValue(binding.lastNameInput.text.toString())
+                                    this@apply.child("user")
+                                        .child(binding.emailInput.text.toString()).child("password")
+                                        .setValue("1111")
+                                    startActivity(Intent(this@SingInActivity,ProfileActivity::class.java))
+                                }
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+
+
+                        })
+                    }
+
+                }
+
+                binding.logIn.apply {
+                    setOnClickListener {
+                        startActivity(Intent(this@SingInActivity,LogInActivity::class.java))
+                    }
+                }
+
+                }
+
+            }
+        }
+
 }
